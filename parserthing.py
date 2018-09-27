1e7e6b0de69e06b9032ee76fcc292a3dd6fb3e1d
superspeeder <andy@delusion.org>
2018-09-27 12:41:12 -0700

    Update parserthing.py

--- parserthing.py
+++ parserthing.py
@@ -1,1 +1,19 @@
 
+test_data = """
+*start [file:<file>]
+
+*start [variables:<varible_declarations>&require:section(functions)]
+<int> x = 10
+<string> string_x = {convert:string(x)}
+*end [variables]
+
+*start [functions:<function_declarations:variants>&require:section(abstracts)]
+#variant<convert@abstracts> string
+return arguments.get_item(0).as_string()
+*end [functions]
+
+*start [abstracts:<function_declarations:abstracts>]
+#function:abstract [convert&require:arguments(1)]
+*end [abstracts]
+
+*end [file]"""
